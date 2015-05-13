af8e1e41325cd8b5fdb11d1974482a37c08a7fe4
Michael Spencer <sonrisesoftware@gmail.com>
2015-05-13 12:11:48 -0500

    / ‘desktop_shell/window_spread.js’

---
+++ desktop_shell/window_spread.js
@@ -0,0 +1,13 @@
+function spread_windows() {
+	// First calculate the number of rows	
+}
+
+function number_of_rows() {
+	// Calculate the total width
+	var base_width = 0
+	var base_height = 1
+
+	for (window in windows) {
+		var width = window.width * base_height/window.height
+	}
+}
